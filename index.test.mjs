34a2ebcd65f2e0532562483be3245966f38022c2
Jen <111.000@0112358.io>
2025-10-27 14:27:13 -0700

    ['👁️', '🐍', '🦩', '🦠', '🌞']

---
+++ index.test.mjs
@@ -0,0 +1,71 @@
+import { Timeless } from './index.mjs'
+
+const ALPHA = `!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_\`abcdefghijklmnopqrstuvwxyz{|}~❤️‍🔥🦃👻🧑‍🧒`
+
+function separator() { 
+  console.log('\n',' 👁️ '.repeat(Math.floor(Math.random() * 30) + 5))
+  console.log('👁️  '.repeat(Math.floor(Math.random() * 45) + 5))
+  console.log('  👁️  '.repeat(Math.floor(Math.random() * 25) + 5), '\n')
+}
+
+function generateText(length, alpha, gr) {
+  let r = ''
+  const a = splitGraphemes(alpha, gr)
+  for (let i = 0; i < length; i++) { r += a[Math.floor(Math.random() * a.length)] }
+  return r
+}
+
+function splitGraphemes(str, gr) {
+  if (typeof Intl !== 'undefined' && 'Segmenter' in Intl) {
+    const sg = new Intl.Segmenter('en', { granularity: 'grapheme' })
+    return Array.from(sg.segment(str), ({ segment }) => segment)
+  } else {
+    const sgs = []
+    let m
+    while ((m = gr.exec(str)) !== null) { sgs.push(m[0]) }
+    return sgs.length > 0 ? sgs : Array.from(str)
+  }
+}
+
+function setEncoder(symbols) {
+  const enc = new Timeless()
+  enc.setSymbols(symbols)
+  enc.setAlphabet(ALPHA)
+  return enc
+}
+
+separator()
+
+const enc = setEncoder(['💎', '🌋', '🏔️', '🪱', '🌳'])
+for (let i = 0; i < Math.floor(Math.random() * 3) + 1; i++) {
+  const message = generateText(Math.floor(Math.random() * 24) + 3, ALPHA, enc.getState().gr)
+  try {
+    console.log(`\nTesting: "${message}"`) 
+    console.log(`\nAlphabet: ${enc.getState().alphabet}`)
+    const en = enc.encodeString(message)
+    console.log(`\nEncoded:\n${en}`)
+    const de = enc.decodeString(en)
+    console.log(`\nDecoded:\n${de}`)
+    console.log('\nSuccess:', message === de ? '✅' : '❌')
+  } catch (e) {
+    console.error(`\nError: ${e.message}`)
+  }
+  separator()
+}
+
+const encF = setEncoder(['🦢', '.'])
+encF.setAlphabet(`01`)
+const tF = ['#HELLO#\\\n##?!_What^❤️‍🔥']
+tF.forEach(m => {
+  try {
+    console.log(`\nTesting: "${m}"`)
+    encF.encodeString(m)
+    console.error(`Fail: ❌`)
+  } catch (e) {
+    console.log(`\nAlphabet: ${encF.getState().alphabet}`)
+    console.error(`\nError: ${e.message}`)
+    console.log('\nSuccess:', e.message ? '✅' : '❌')
+  }
+})
+
+separator()
